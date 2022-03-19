7dda0fdd097b04890e88ce59830dc9477b08db9b
Jorge-Ortiz <ortiz.mata.jorge@gmail.com>
2022-03-19 13:08:42 -0600

    Styling app and adding stocks model and controller.

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
-  devise_for :users
-  root 'pages#home'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  # Stocks controller routes.
+  get 'stocks/search'
+
+  # User devise routes.
+  devise_for :users
+
+  # Root path.
+  root 'pages#home'
+
 end
